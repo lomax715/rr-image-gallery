24f4f814d60b77251116ac863e51cab25d275ce0
Charlie Heiner <heinercj@gmail.com>
2018-03-06 09:34:23 -0800

    api actions

--- src/components/images/actions.js
+++ src/components/images/actions.js
@@ -1,15 +1,23 @@
 import imageApi from '../../services/imageApi';
-import { IMAGE_LOAD } from './reducers';
+import { IMAGE_LOAD, IMAGE_ADD, IMAGE_DELETE } from './reducers';
 
 export function loadImages(){
-  return dispatch => {
-    return imageApi.load()
-      .then(images => {
-        dispatch({
-          type: IMAGE_LOAD,
-          payload: images
-        });
-      });
+  return {
+    type: IMAGE_LOAD,
+    payload: imageApi.load()
   };
+}
 
+export function addImage(imgObj){
+  return {
+    type: IMAGE_ADD,
+    payload: imageApi(imgObj)
+  };
+}
+
+export function removeImage(imageId){
+  return {
+    type: IMAGE_DELETE,
+    payload: imageApi(imageId).then(() => imageId)
+  };
 }
