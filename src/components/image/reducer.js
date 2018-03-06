7b99c05655436dabbcca34d2130e2a90818abdb5
G.Provost <grace.g.provost@gmail.com>
2018-03-05 20:10:01 -0800

    create image reducers

--- src/components/image/reducer.js
+++ src/components/image/reducer.js
@@ -1,3 +1,19 @@
 export const IMAGE_ADD = 'IMAGE_ADD';
 export const IMAGE_REMOVE = 'IMAGE_REMOVE';
 export const IMAGE_LOAD = 'IMAGE_LOAD';
+
+export function images(state = [], { type, payload }) {
+  switch(type) {
+    case IMAGE_LOAD: 
+      return payload;
+    case IMAGE_ADD:
+      return [
+        ...state,
+        payload
+      ];
+    case IMAGE_REMOVE:
+      return state.filter(image => image.id !== payload);
+    default:
+      return state;
+  }
+}
--- src/components/image/reducer.test.js
+++ src/components/image/reducer.test.js
@@ -1,1 +1,33 @@
-import { images, IMAGE_ADD, IMAGE_REMOVE, IMAGE_LOAD } from './reducers';
+import { images, IMAGE_ADD, IMAGE_REMOVE, IMAGE_LOAD } from './reducer';
+
+const imageToAdd = {
+  id: 111,
+  title: 'test',
+  url: 'test.png',
+  album: 123,
+  description: 'testing images'
+};
+
+it('loads images', () => {
+  const imagesToLoad = [
+    imageToAdd,
+    { id: 121, title: 'sneakers', url: 'nope.jpg', album: 123, description: 'swampy' }
+  ];
+  const state = images([], { type: IMAGE_LOAD, payload: imagesToLoad });
+  expect(state).toEqual(imagesToLoad);
+});
+
+it('default empty array', () => {
+  const state = images(undefined, {});
+  expect(state).toEqual([]);
+});
+
+it('adds an image', () => {
+  const state = images([], { type: IMAGE_ADD, payload: imageToAdd });
+  expect(state).toEqual([imageToAdd]);
+});
+
+it('removes an image', () => {
+  const state = images([imageToAdd], { type: IMAGE_REMOVE, payload: 111 });
+  expect(state).toEqual([]);
+});
